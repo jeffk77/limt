96fe807518a4dfd91bb2c1d079731ee5e31d22c9
Kwong <aykwong93@gmail.com>
2018-09-15 11:34:05 -0400

    Added products testing, fixed datatypes in tables

--- models/products.js
+++ models/products.js
@@ -8,7 +8,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     base_price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         len: [1]
