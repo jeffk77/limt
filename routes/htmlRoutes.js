c47ad0416183c2d8ef16c77898bff09ca59431e5
unknown <msvendsentan@gmail.com>
2018-09-15 12:18:33 -0400

    html routes cleaned up, handlebars pages cleaned up, skeleton pages put up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app, passport) {
   // Load index page
-  app.get("/", function(req, res) {
+/*   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
@@ -10,39 +10,47 @@ module.exports = function(app, passport) {
       });
     });
   });
-
+ */
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+/*   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
-
-  app.get("/authex", function(req, res) {
-    res.render("authex", { user: req.user });
+ */
+  app.get("/", function(req, res) {
+    res.render("index", { user: req.user });
   });
-  
+
+  app.get("/products", function(req, res) {
+    res.render("products");
+  });
+
+  app.get("/order", function(req, res) {
+    res.render("order");
+  });
+
   app.get("/login", function(req, res) {
     res.render("login");
-  });
+  })
   
   app.post("/login", passport.authenticate("local", { failureRedirect: "/login" }), function(req, res) {
-    res.redirect("/authex");
+    res.redirect("/");
   });
   
   app.get("/logout", function(req, res) {
     req.logout();
-    res.redirect("/authex");
+    res.redirect("/");
   });
   
-  app.get("/secret", require("connect-ensure-login").ensureLoggedIn(), function(req, res) {
-    res.render("secret", { user: req.user });
+  app.get("/admin", require("connect-ensure-login").ensureLoggedIn(), function(req, res) {
+    res.render("admin", { user: req.user });
   });
 
   // Render 404 page for any unmatched routes
-/*   app.get("*", function(req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
-  }); */
+  });
 };
